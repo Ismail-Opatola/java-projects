9a85260689ae6047b9a16be21fc1a952589f3e69
Ismail Opatola <opatolamails@gmail.com>
2021-04-04 21:40:55 +0100

    a simple encryption program

---
+++ Encryption/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * OTP = One-time pad
+		 * 
+		 * encryption technique that cannot be cracked,
+		 * but requires the use of a one-time pre-shared
+		 * key the same size as, or longer than, the message
+		 * being sent. jTn this technique, plaintext is pared
+		 * with a random secret key (also reffered to as a one-time pad).
+		 */
+		
+		// A simple encryption program
+		EncryptionProgram ep = new EncryptionProgram();
+
+	}
+
+}
