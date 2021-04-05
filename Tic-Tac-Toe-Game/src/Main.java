de0c96c6877c6f93b19e5f18db198e77f15cf283
Ismail Opatola <opatolamails@gmail.com>
2021-04-05 20:46:53 +0100

    tic-tac-teo game

---
+++ Tic-Tac-Toe-Game/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * A tic-tac-toe game
+		 * ------------------
+		 */
+
+		TicTacToe ticTacToe = new TicTacToe();
+	}
+
+}
